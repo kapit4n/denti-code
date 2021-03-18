1e59e1d9ec2a1c806a9cd1beb7019e5954ba24d1
Luis Arce <luis.arce22@gmail.com>
2021-03-18 00:06:50 -0400

    ui: basic structure nav

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ClientsList from './views/clients/list'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom"
+
 import Nav from './Nav'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Router>
         <Nav />
-      </header>
+        <Switch>
+          <Route path="clients">
+            <ClientsList />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- ui/src/Nav.js
+++ ui/src/Nav.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 export default function Nav() {
 
-  const options = ['doctor', 'clients', 'records'];
+  const options = ['doctors', 'clients', 'records', 'bookings'];
 
   return (
-    <div>
-      {options.map(o => <span>{o} | </span>)}
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <ul style={{ display: "flex", justifyContent: 'flex-start', listStyleType: 'none' }}>
+        {options.map(o => <li style={{ padding: '0.5rem' }}><Link to={o}>{o}</Link></li>)}
+      </ul>
+      <div style={{ padding: '1.5rem 2rem' }}>
+        <Link to="login">Login</Link>
+      </div>
     </div>
   )
 }
