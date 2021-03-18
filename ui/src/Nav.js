09798d3a277ed5684d3784e23878878c2d5efd10
Luis Arce <luis.arce22@gmail.com>
2021-03-18 00:36:08 -0400

    ui: Drawer initial

--- ui/src/Nav.js
+++ ui/src/Nav.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Link } from 'react-router-dom';
+import Drawer from '@material-ui/core/Drawer';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu'
+import IconButton from '@material-ui/core/IconButton'
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  drawer: {
+    width: 100,
+    flexShrint: 0,
+  }
+}))
 
 export default function Nav() {
 
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
   const options = ['doctors', 'clients', 'records', 'bookings'];
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  }
 
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-      <ul style={{ display: "flex", justifyContent: 'flex-start', listStyleType: 'none' }}>
-        {options.map(o => <li style={{ padding: '0.5rem' }}><Link to={o}>{o}</Link></li>)}
-      </ul>
+      <Drawer variant="persistent" archor="left" open={open} classes={{ paper: classes.drawePaper }}>
+        <ul>
+          {options.map(o => <li style={{ padding: '0.5rem' }}><Link to={o}>{o}</Link></li>)}
+        </ul>
+      </Drawer>
+      <div>
+        <Toolbar>
+          <IconButton color="inherit" aria-label="open drawer" onClick={handleDrawerOpen} edge="start">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </div>
       <div style={{ padding: '1.5rem 2rem' }}>
         <Link to="login">Login</Link>
       </div>
