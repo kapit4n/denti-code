c33cfbaf07b65c4debcfb216f1995df511248f20
Luis Arce <luis.arce22@gmail.com>
2021-04-20 21:33:29 -0400

    Render login and password form controls

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -6,3 +6,11 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/Login/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders User Name Form Control', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Login/i)
+  fireEvent.click(linkElement)
+  expect(screen.getByText(/User Name/i)).toBeTruthy()
+  expect(screen.getByText(/Password/i)).toBeTruthy()
+})
