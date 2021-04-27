44992cbad98a1f20124a753dd0abb100cc970730
Luis Arce <luis.arce22@gmail.com>
2021-04-26 20:09:19 -0400

    ui: details

--- ui/src/views/clients/details/index.js
+++ ui/src/views/clients/details/index.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardHeader from '@material-ui/core/CardHeader'
+import axios from 'axios'
 
 export default function Details() {
 
   const { id } = useParams();
+  const [client, setClient] = React.useState({})
+
+  useEffect(async () => {
+    const result = await axios.get(`http://localhost:3000/users/${id}`)
+    setClient(result.data)
+  }, [id])
 
   return (
-    <div>
-      {id}
-    </div>
+    <Card>
+      <CardHeader title={client.firstName} />
+      <CardContent>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+        tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+        quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+        sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+        recusandae alias error harum maxime adipisci amet laborum. Perspiciatis
+        minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
+        quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur
+        fugiat, temporibus enim commodi iusto libero magni deleniti quod quam
+        consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+        doloremque. Quaerat provident commodi consectetur veniam similique ad
+        earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
+        fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore
+        suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+        modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam
+        totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam
+        quasi aliquam eligendi, placeat qui corporis!
+      </CardContent>
+    </Card>
   )
 }
