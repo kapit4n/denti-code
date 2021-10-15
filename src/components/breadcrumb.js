d58a1e04e349dba2fe481a0e6f4227fe9f20b580
Luis Arce <luis.arce22@gmail.com>
2021-10-15 11:39:38 -0400

    breadcrumbs: fix it

--- src/components/breadcrumb.js
+++ src/components/breadcrumb.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import MLink from '@mui/material/Link';
 
 export default function Index({ items }) {
   return (
     <div>
-      {items.map(i => <><Link to={i.route}>{i.label}</Link> / </>)}
+
+      <Breadcrumbs aria-label="breadcrumb">
+        {items.map(i => (
+          <>
+            {i.route ? (
+              <Link
+                underline="hover"
+                color="inherit"
+                to={i.route}
+              >
+                {i.label}
+              </Link>
+            ) : (
+              <Typography color="text.primary">{i.label}</Typography>
+            )}
+          </>
+        ))
+        }
+      </Breadcrumbs>
     </div>
   )
 }
