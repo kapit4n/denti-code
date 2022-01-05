e1ec1007bd269e42f79f989c8a64291c1932c0a1
Luis Arce <luis.arce22@gmail.com>
2022-01-05 10:24:06 -0400

    Doctors: Add create action button

--- src/views/doctors/list/main.js
+++ src/views/doctors/list/main.js
@@ -1,41 +1,56 @@
 import React from 'react'
 
-import { List, ListItem, ListItemText, ListItemSecondaryAction, IconButton, Divider } from '@mui/material/'
+import {
+  List, ListItem,
+  ListItemText, ListItemSecondaryAction,
+  IconButton, Divider, Button
+} from '@mui/material/'
 
 import OpenIcon from '@material-ui/icons/FolderOpen'
 import DeleteIcon from '@material-ui/icons/DeleteForever'
 
 import './main.css'
+import { Link } from 'react-router-dom'
 
 export default function Main({ clients, goToItem, onRemove }) {
   return (
-    <List>
-      {clients && clients.map(c => (
-        <div>
-          <ListItem key={c.id}>
-            <ListItemText>
-              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <div>
-                  <h2> {`Name: ${c.firstName} ${c.lastName}`} </h2>
-                  <div>{`Speciality: ${c.speciality}`}</div>
-                  <div>{`CreateAt: ${c.createdAt}`}</div>
-                </div>
-              </div>
-            </ListItemText>
-            <ListItemSecondaryAction>
-              <IconButton onClick={() => goToItem(c)}>
-                <OpenIcon />
-              </IconButton>
-              
-              <IconButton onClick={() => onRemove(c.id)}>
-                <DeleteIcon />
-              </IconButton>
-
-            </ListItemSecondaryAction>
-          </ListItem>
-          <Divider variant="inset" component="li" style={{ marginLeft: 0 }} />
+    <>
+      <div className="list-header">
+        <div className="list-header-actions">
+          <Button><Link to="/doctors/create">Create</Link></Button>
         </div>
-      ))}
-    </List>
+        <div className="list-header-filters">
+          <Button >Filters</Button>
+        </div>
+      </div>
+      <List>
+        {clients && clients.map(c => (
+          <div>
+            <ListItem key={c.id}>
+              <ListItemText>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                  <div>
+                    <h2> {`Name: ${c.firstName} ${c.lastName}`} </h2>
+                    <div>{`Speciality: ${c.speciality}`}</div>
+                    <div>{`CreateAt: ${c.createdAt}`}</div>
+                  </div>
+                </div>
+              </ListItemText>
+              <ListItemSecondaryAction>
+                <IconButton onClick={() => goToItem(c)}>
+                  <OpenIcon />
+                </IconButton>
+
+                <IconButton onClick={() => onRemove(c.id)}>
+                  <DeleteIcon />
+                </IconButton>
+
+              </ListItemSecondaryAction>
+            </ListItem>
+            <Divider variant="inset" component="li" style={{ marginLeft: 0 }} />
+          </div>
+        ))}
+      </List>
+    </>
   )
 }
