afd5dbe5cd0a66159da602b68b3fcaf11d886e26
Luis Arce <luis.arce22@gmail.com>
2023-05-15 21:20:15 -0400

    ui: Use token in api fetching

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 https://github.com/kapit4n/denti-code-server
 
 ![Dashboard](https://github.com/kapit4n/denti-code/blob/master/screenshots/main4.gif)
+
+## TODO Tasks
+- Have a common list items
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,17 +1,24 @@
 import { useState, useEffect } from "react";
 import axios from 'axios'
+import { useCookies } from "react-cookie"
+
 
 export default function ({ entity }) {
   const [data, setData] = useState([])
+  const [cookies] = useCookies(['token'])
   const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(async () => {
+  useEffect(() => {
+    let token = ""
+    if (cookies) {
+      token = cookies["token"]
+    }
     (async function () {
-        const list = await axios.get(`${process.env.REACT_APP_API_PATH}/${entity}/`)
-        setData(list.data)
-        setIsLoading(false)
-      })();
-  }, [entity])
+      const list = await axios.get(`${process.env.REACT_APP_API_PATH}/${entity}/?token=${token}`)
+      setData(list.data)
+      setIsLoading(false)
+    })();
+  }, [entity, cookies])
 
   return { data, isLoading, setData }
 }
